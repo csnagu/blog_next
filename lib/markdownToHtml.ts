7785a335fc4df3f71983c12d773333883a39523a
Chikara Saito <s.chikara3110@gmail.com>
2021-11-14 17:04:09 +0900

    prisma対応

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,12 +1,12 @@
 import { remark } from 'remark'
 import html from 'remark-html'
-import prism from 'remark-prism'
+// import prism from 'remark-prism'
 import gfm from 'remark-gfm'
 import remarkSlug from 'remark-slug'
 import remarkToc from 'remark-toc'
 
 export default async function markdownToHtml(raw_markdown: string) {
   const toc_markdown = "## 目次\n" + raw_markdown
-  const result = await remark().use(gfm).use(html, { sanitize: false }).use(prism).use(remarkSlug).use(remarkToc, { heading: "目次" }).process(toc_markdown)
+  const result = await remark().use(gfm).use(html, { sanitize: false }).use(require('remark-prism')).use(remarkSlug).use(remarkToc, { heading: "目次" }).process(toc_markdown)
   return result.toString()
 }
